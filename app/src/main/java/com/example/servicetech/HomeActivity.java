6295d41e3552e8233897e378b19624ed4934955d
devAndie <andrewfundikabiru@gmail.com>
2020-11-20 01:48:02 +0300

    rapid build

--- app/src/main/java/com/example/servicetech/HomeActivity.java
+++ app/src/main/java/com/example/servicetech/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.servicetech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,22 +19,38 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button login = findViewById(R.id.log1);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loginIntent = new Intent(getApplicationContext(), LogInActivity.class);
-                startActivity(loginIntent);
-            }
-        });
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        Button signup = findViewById(R.id.signUp);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signupIntent = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(signupIntent);
-            }
-        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFrag = null;
+
+                    switch (item.getItemId()){
+                        case  R.id.home:
+                            selectedFrag = new HomeFragment();
+                            break;
+                        case  R.id.dash:
+                            selectedFrag = new DashboardFragment();
+                            break;
+                        case  R.id.event:
+                            selectedFrag = new EventFragment();
+                            break;
+                        case  R.id.about:
+                            selectedFrag = new AboutFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFrag).commit();
+
+                    return  true;
+                }
+            };
 }
