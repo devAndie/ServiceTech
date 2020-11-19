5714d699d48b35453ed01ad2ebb8f85f3fc5314f
devAndie <andrewfundikabiru@gmail.com>
2020-11-20 00:06:45 +0300

    9t 9t

--- app/src/main/java/com/example/servicetech/HomeActivity.java
+++ app/src/main/java/com/example/servicetech/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.servicetech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button login = findViewById(R.id.log1);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(), LogInActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        Button signup = findViewById(R.id.signUp);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signupIntent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(signupIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/servicetech/LogInActivity.java
+++ app/src/main/java/com/example/servicetech/LogInActivity.java
@@ -2,7 +2,10 @@ package com.example.servicetech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button logIn =findViewById(R.id.log2);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+
     }
 }
