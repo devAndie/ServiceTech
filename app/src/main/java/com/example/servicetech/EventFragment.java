c3f069cc8ec514b57818c73acffa885e4702eaa5
devAndie <andrewfundikabiru@gmail.com>
2020-11-20 17:27:58 +0300

    frags

--- app/src/main/java/com/example/servicetech/EventFragment.java
+++ app/src/main/java/com/example/servicetech/EventFragment.java
@@ -9,11 +9,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class EventFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
 }
