5ee9dae527e461f33b3937a4b5ef67e9b1eea381
devAndie <andrewfundikabiru@gmail.com>
2020-11-20 21:07:09 +0300

    pass

--- app/src/main/java/com/example/servicetech/SignInActivity.java
+++ app/src/main/java/com/example/servicetech/SignInActivity.java
@@ -9,6 +9,17 @@ import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
+    public static final int MY_PASSWORD_DIALOG_ID = 4;
+    public static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            "(?=.*[0-9])" +         //at least 1 digit
+            "(?=.*[a-z])" +         //at least 1 lower case letter
+            "(?=.*[A-Z])" +         //at least 1 upper case letter
+            "(?=.*[a-zA-Z])" +      //any letter
+            "(?=.*[@#$%^&+=])" +    //at least 1 special character
+            "(?=\\S+$)" +           //no white spaces
+            ".{4,}" +               //at least 4 characters
+            "$");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
