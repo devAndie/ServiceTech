04b748b2c51cb5305ffe468ae7fc55d88b00f1c9
devAndie <andrewfundikabiru@gmail.com>
2020-12-19 21:16:45 +0300

    db h

---
+++ app/src/main/java/UserDBHelper.java
@@ -0,0 +1,21 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UserDBHelper extends SQLiteOpenHelper {
+    public UserDBHelper(@Nullable Context context) {
+        super(context, "", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
